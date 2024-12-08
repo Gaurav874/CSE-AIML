e5d0bc56f9720aeb8f5e8b6ea7a58ab5befa6571
Gaurav <gk1500874@gmail.com>
2024-12-08 18:07:33 +0530

    js code

--- fetch/practise1.js
+++ fetch/practise1.js
@@ -10,14 +10,12 @@
 
 
 function getData(){
-    fetch("https://api.github.com/users/Gaurav874").then((response)=>{
-      const data = response.json();
-      console.log(data)
-     return data;
-    }).then((data)=>{
+    fetch("https://api.github.com/users/Gaurav874")
+    .then((response) => response.json())
+    .then((data)=>{
         const div = document.getElementById("parent");
-        const av = document.getElementById("avatar");
-        av.style.display = "none";
+        // const av = document.getElementById("avatar");
+        // av.style.display = "none";
         div.innerHTML = `<h2>${data.name}</h2>
         <p><strong>Username:</strong> ${data.login}</p>
         <p><strong>bio:</strong> ${data.bio}</p>
@@ -26,6 +24,18 @@ function getData(){
 
     })
 }
+
+
+
+
+
+
+// 1. function call
+// 2. fetch AudioParam
+// 3. response ko json me convert 
+// 4. response ka jo data hn uski jaga bnana aur value print krna
+// 5. error ko catch krna 
+// 6. clear function call
 
 
 
@@ -52,3 +62,32 @@ function getData(){
 //       <img src="${data.avatar_url}" alt="Avatar" width="150" />
 //     `;
 //   };
+
+
+
+
+
+
+
+
+
+
+
+
+// function getData(){
+//     fetch("https://api.github.com/users/Gaurav874")
+//       .then((response)=>{const data = response.json();
+//       console.log(data)
+//      return data;
+//     }).then((data)=>{
+//         const div = document.getElementById("parent");
+//         const av = document.getElementById("avatar");
+//         av.style.display = "none";
+//         div.innerHTML = `<h2>${data.name}</h2>
+//         <p><strong>Username:</strong> ${data.login}</p>
+//         <p><strong>bio:</strong> ${data.bio}</p>
+//         <p><strong>public repo:</strong> ${data.public_repos}</p>
+//         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcyIzz2XHx17Wzb1GmWtxQrJG50YA5xVpGzg&s"/>`
+
+//     })
+// }
--- fsd_test/api_fetch.js
+++ fsd_test/api_fetch.js
@@ -1,21 +1,54 @@
-function getData() {
+function getData(){
     fetch("https://v2.jokeapi.dev/joke/Any")
-        .then((response) => response.json())
-        .then((data) => {
-            const div = document.getElementById('jokeOutput');
-           
-            if (data.type === "single") {
-                div.innerHTML = `<p>${data.joke}</p>`;
-            } else if (data.type === "twopart") {
-                div.innerHTML = `<p>${data.setup} - ${data.delivery}</p>`;
-            }
-        })
-        .catch((error) => {
-            console.error("Error fetching joke:", error);
-            document.getElementById('jokeOutput').innerHTML = 'Failed to load a joke. Please try again.';
-        });
+    .then((response => response.json()))
+    .then((data) => {
+        const div = document.getElementById('jokeOutput');
+        if(data.type === "single"){
+            div.innerHTML = `${data.joke}`
+        }
+        else if(data.type === "twopart"){
+            div.innerHTML == `${data.setup} - ${data.delivery}`
+        }
+    })
+    .catch((error) => {
+        div.innerHTML = 'Failed to load this page...'
+    })
+    function clearJoke(){
+        div.innerHTML = "";
+    }
 }
 
-function clearJoke() {
-    document.getElementById('jokeOutput').innerHTML = '';
-}
+
+
+
+
+
+
+
+
+
+
+
+
+// function getData() {
+//     fetch("https://v2.jokeapi.dev/joke/Any")
+//         .then((response) => response.json())
+//         .then((data) => {
+//             const div = document.getElementById('jokeOutput');
+           
+//             if (data.type === "single") {
+//                 div.innerHTML = `<p>${data.joke}</p>`;
+//             } else if (data.type === "twopart") {
+//                 div.innerHTML = `<p>${data.setup} - ${data.delivery}</p>`;
+//             }
+
+//         })
+//         .catch((error) => {
+//             console.error("Error fetching joke:", error);
+//             document.getElementById('jokeOutput').innerHTML = 'Failed to load a joke. Please try again.';
+//         });
+// }
+
+// function clearJoke() {
+//     document.getElementById('jokeOutput').innerHTML = '';
+// }
